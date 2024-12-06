ded4f3fc870e382a1dc6c81a1263026a94f04a39
codexrafiullah <codexrafiullah@mail.com>
2024-12-06 03:20:07 -0800

    uploading code

--- Test/test.js
+++ Test/test.js
@@ -28,16 +28,51 @@
 // }
 
 
-function table() {
-  for (let i = 1; i <= 10; i++) {
-    let result = number * i;
-    if (result % 2 === 0) {
-      console.log(result, "even number");
-    } else {
-      console.log(result, "odd number");
-    }
-  }
+function table(){
+  let number = 3;
+let result = 0;
+for (let i=1; i<=10; i++){
+     result = number * i;
+     if(result%2===0){
+        console.log(result,"even number");
+     }else{
+        console.log(result,"odd number");
+     }
 }
-
-let number = parseInt(prompt("Enter a number to print a Table"));
+}
+let number = 3;
 table();
+
+
+// const condition = (result) => {
+//   if (result % 2 === 0) {
+//     return(result + "  even number");
+//   } else {
+//     return(result + "  odd number");
+//   }
+// }
+
+// function table() {
+//   for (let i = 1; i <= 10; i++) {
+//     let result = number * i;
+//     console.log(condition(result));
+//   }
+// }
+
+// let number = 3;
+// table();
+
+
+
+// let arr1 = [1,2,3,4,5,6,7,8,9];
+
+// let evenArr = arr1.filter((val) => {
+//   return val%2===0;
+// })
+// console.log(evenArr);
+
+// let arr2 = [1,2,3,4,5,6,7,8,9];
+// let evenNum = arr2.filter(val => val%2 === 0)
+// console.log(evenNum);
+
+
